8c13bfafe7ee6a95806d36b4ae227673b8f2f451
RAFH82 <hrechkar@gmail.com>
2021-05-26 13:02:47 -0700

    almost done front end

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,7 +1,19 @@
-function ProductFeed() {
+import Product from "./Product";
+
+function ProductFeed({products}) {
 	return (
 		<div>
 			<h1>Products ...</h1>
+			{products.map(({id, title, price, description, category, image}) => (
+				<Product
+					key={id}
+					title={title}
+					price={price}
+					description={description}
+					category={category}
+					image={image}
+				/>
+			))}
 		</div>
 	);
 }
