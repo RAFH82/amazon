f62d7161e198e18bedd547b16af105fae76cf6c6
RAFH82 <hrechkar@gmail.com>
2021-05-26 13:50:55 -0700

    finished product feed

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,34 +10,40 @@ function Product({id, title, price, description, category, image}) {
 	const [rating] = useState(
 		Math.floor(Math.random() * (MAX_RATING - MAX_RATING + 1)) + MIN_RATING
 	);
-
 	const [hasPrime] = useState(Math.random() < 0.5);
 
 	return (
-		<div>
-			<p>{category}</p>
+		<div className="relative flex flex-col bg-white m-5 z-30 p-10">
+			<p className="absolute top-2 right-2 text-xs italic text-gray-400 ">
+				{category}
+			</p>
 
 			<Image src={image} height={200} width={200} objectFit="contain" />
-			<h4>{title}</h4>
+
+			<h4 className="my-3">{title}</h4>
 
 			<div className="flex">
 				{Array(rating)
 					.fill()
 					.map((_, i) => (
-						<StarIcon key={i} className="h-5" />
+						<StarIcon key={i} className="h-5 text-yellow-500" />
 					))}
 			</div>
 
-			<p>{description}</p>
-			<div>
+			<p className="text-xs my-2 line-clamp-2">{description}</p>
+
+			<div className="mb-5">
 				<Currency quantity={price} currency="CAD" />
 			</div>
 
 			{hasPrime && (
-				<div>
-					<img src="https://links.papareact.com/fdw" alt="" />
+				<div className="flex items-center space-x-2 -mt-5">
+					<img className="w-12" src="https://links.papareact.com/fdw" alt="" />
+					<p className="text-xs">FREE Next-day Delivery</p>
 				</div>
 			)}
+
+			<button className="mt-auto button">Add to Basket</button>
 		</div>
 	);
 }
